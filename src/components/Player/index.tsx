a7c7b5ea6b8d1f48c0071967897ed067ee83e9c5
Guilherme Bafica <gb.engcomp@hotmail.com>
2021-04-27 10:09:27 -0300

    CHORE: Loop control

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -15,7 +15,9 @@ export function Player() {
     episodeList,
     currentEpisodeIndex,
     isPlaying,
+    isLooping,
     togglePlay,
+    toggleLoop,
     setPlayingState,
     playNext,
     playPrevious,
@@ -82,6 +84,7 @@ export function Player() {
           <audio
             src={episode.url}
             autoPlay
+            loop={isLooping}
             ref={audioRef}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
@@ -113,7 +116,12 @@ export function Player() {
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
 
-          <button type="button" disabled={!episode} >
+          <button
+            type="button"
+            disabled={!episode}
+            onClick={toggleLoop}
+            className={isLooping ? styles.isActive : ''}
+          >
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
